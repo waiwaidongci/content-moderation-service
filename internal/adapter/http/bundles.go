9d6a4226865b573ee18af249e8a3f3e9590c33ae
waiwaidongci <786958579@qq.com>
2026-08-27 05:57:08 +0800

    test: add acceptance checks

---
+++ internal/adapter/http/bundles.go
@@ -0,0 +1,66 @@
+package httpadapter
+
+import (
+	"context"
+	"github.com/ali/go-0821/content-moderation-service/internal/domain"
+	"net/http"
+)
+
+func (s *Server) moderationWorkspaces(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.WriteHeader(405)
+		return
+	}
+	var v domain.ModerationWorkspace
+	if err := decode(r, &v); err != nil {
+		writeErr(w, err)
+		return
+	}
+	if err := s.bundles.CreateModerationWorkspace(context.Background(), v); err != nil {
+		writeErr(w, err)
+		return
+	}
+	writeJSON(w, 201, v)
+}
+func (s *Server) moderationChannels(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		o, e := s.bundles.ListModerationChannels(r.Context(), r.URL.Query().Get("workspace_id"))
+		if e != nil {
+			writeErr(w, e)
+			return
+		}
+		writeJSON(w, 200, o)
+		return
+	}
+	var v domain.ModerationChannel
+	if err := decode(r, &v); err != nil {
+		writeErr(w, err)
+		return
+	}
+	if err := s.bundles.CreateModerationChannel(context.Background(), v); err != nil {
+		writeErr(w, err)
+		return
+	}
+	writeJSON(w, 201, v)
+}
+func (s *Server) ruleBundles(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		o, e := s.bundles.ListRuleBundles(r.Context(), r.URL.Query().Get("workspace_id"), r.URL.Query().Get("channel_id"))
+		if e != nil {
+			writeErr(w, e)
+			return
+		}
+		writeJSON(w, 200, o)
+		return
+	}
+	var v domain.RuleBundle
+	if err := decode(r, &v); err != nil {
+		writeErr(w, err)
+		return
+	}
+	if err := s.bundles.CreateRuleBundle(context.Background(), v); err != nil {
+		writeErr(w, err)
+		return
+	}
+	writeJSON(w, 201, v)
+}
